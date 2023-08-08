cb353d2495450dccf051c9b20a98f79716b3d8cf
Tan Nguyen <hi@tan.ng>
2023-08-08 15:02:30 +0700

    fix: file and file upload

--- src/Templates/File.php
+++ src/Templates/File.php
@@ -9,6 +9,10 @@ class File extends Base {
 		if ( empty( $value ) || !is_array($value) ) {
 			return '';
 		}
+
+		if (is_array($value)) {
+			$value = reset($value);
+		}
 		
         return "<a href='{$value['url']}' target='_blank'>{$value['title']}</a>\n";
 	}
